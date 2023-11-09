bd06f8319a552bd10afc3adce3e6acc97addbd5b
nmd2000 <manhdung20112000@gmail.com>
2023-11-09 17:33:21 +0700

    step 2

--- build-ml-pipeline-for-short-term-rental-prices/src/basic_cleaning/run.py
+++ build-ml-pipeline-for-short-term-rental-prices/src/basic_cleaning/run.py
@@ -5,7 +5,7 @@ Performs basic cleaning on the data and save the results in Weights & Biases
 import argparse
 import logging
 import wandb
-
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -18,7 +18,31 @@ def go(args):
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    
+    ######################
+    # YOUR CODE HERE     #
+    ######################
+    
+    df = pd.read_csv(artifact_local_path, index_col="id")
+    logger.info("Loaded data")
+    
+    indexes = df['price'].between(args.min_price, args.max_price)
+    df = df[indexes].copy()
+    
+    logger.info("Dataset price outliers removal outside range:" + \
+                f"{args.min_price}-{args.max_price}")
+
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    logger.info("Converted last_review to datetime")
+
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
+    df.to_csv(args.output_artifact)
+
+    # Upload artifact
     artifact = wandb.Artifact(
         args.output_artifact,
         type=args.output_type,
@@ -27,38 +51,16 @@ def go(args):
     artifact.add_file("clean_sample.csv")
     run.log_artifact(artifact)
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
-    
-
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description="This steps cleans the data")
 
-
-    parser.add_argument(
-        "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
-    )
-
-    parser.add_argument(
-        "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
-    )
-
-    parser.add_argument(
-        "--parameter3", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
-    )
-
+    parser.add_argument("--input_artifact", type=str)
+    parser.add_argument("--output_artifact", type=str)
+    parser.add_argument("--output_type", type=str)
+    parser.add_argument("--output_description", type=str)
+    parser.add_argument("--min_price", type=float, required=True)
+    parser.add_argument("--max_price", type=float,)
 
     args = parser.parse_args()
 
