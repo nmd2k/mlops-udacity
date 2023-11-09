55f254fa9e6aae81a8922d94c84f739949704542
nmd2000 <manhdung20112000@gmail.com>
2023-11-10 00:27:34 +0700

    update test

--- build-ml-pipeline-for-short-term-rental-prices/main.py
+++ build-ml-pipeline-for-short-term-rental-prices/main.py
@@ -136,7 +136,15 @@ def go(config: DictConfig):
             ##################
             # Implement here #
             ##################
-
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                'main',
+                version='main',                
+                parameters={
+                    'mlflow_model': 'random_forest_export:prod',
+                    'test_dataset': 'test_data.csv:latest'
+                },
+            )
             pass
 
 
